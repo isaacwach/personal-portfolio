ab70f59a23b3d3471e196502429f144cdf3ee4da
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-05-18 02:31:04 +0300

    added getform form action

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import Logo from '../assets/logo.jpeg';
-import {HiOutlineMail} from 'react-icons/hi'
-import { BsFillPersonLinesFill} from 'react-icons/bs'
-
+import {HiOutlineMail} from 'react-icons/hi';
+import { BsFillPersonLinesFill} from 'react-icons/bs';
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -15,11 +15,31 @@ const Navbar = () => {
                 <img src={Logo} alt='log image' style={{ width: '50px' }} />
             </div>
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                <Link to='home' smooth={true} duration={500}>
+                    Home
+                </Link>
+                </li>
+                <li>
+                    <Link to='about' smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to='skills' smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to='work' smooth={true} duration={500}>
+                        Projects
+                    </Link>
+                </li>
+                <li>
+                    <Link to='contact' smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
             {/* hamberger */}
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-            <form action='' className='flex flex-col max-w-[600px] w-full'>
+            <form method='POST' action='https://getform.io/f/b1c65dac-7055-4ebb-81d5-184fa9ac9e00' className='flex flex-col max-w-[600px] w-full'>
                 <div>
                   <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                   <p className='text-gray-300 py-4'>Fill in the form below to contact me or email me via isaacwachira57@gmail.com</p>
